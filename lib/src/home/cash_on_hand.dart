5428f4998412c7c259d0d82b6c35bbd22fd2b151
Konrad Kale <konradk@Konrads-MacBook-Pro.local>
2024-10-06 21:48:21 +0700

    update total v3

--- lib/src/home/cash_on_hand.dart
+++ lib/src/home/cash_on_hand.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'calendar_page.dart';
 import 'event_model.dart';
-import 'package:intl/intl.dart'; // Add this import
 
 
 class CashOnHandPage extends StatefulWidget {
@@ -17,30 +17,35 @@ class _CashOnHandPageState extends State<CashOnHandPage> {
   late DateTime _now;
   late DateTime _endOfWeek;
   late DateTime _endOfMonth;
-  late Map<String, double> _totals;
+  late DateTime _endOfYear;
+  late Map<String, Map<String, double>> _totals;
   final currencyFormatter = NumberFormat("#,##0.00", "en_US");
+    final dateFormatter = DateFormat("EEEE, MMM d, yyyy");
 
   @override
   void initState() {
     super.initState();
     _now = DateTime.now();
-        _endOfWeek = _getEndOfWeek(_now);
-            _endOfMonth = _getEndOfMonth(_now);
+    _endOfWeek = _getEndOfWeek(_now);
+    _endOfMonth = _getEndOfMonth(_now);
+    _endOfYear = DateTime(_now.year, 12, 31);
     _calculateTotals();
   }
 
     DateTime _getEndOfWeek(DateTime date) {
     return date.add(Duration(days: DateTime.saturday - date.weekday + (date.weekday == DateTime.sunday ? 7 : 0)));
   }
+
   DateTime _getEndOfMonth(DateTime date) {
     return DateTime(date.year, date.month + 1, 0);
   }
+
   void _calculateTotals() {
     _totals = {
-      'day': 0,
-      'week': 0,
-      'month': 0,
-      'year': 0,
+      'day': {'positive': 0, 'negative': 0},
+      'week': {'positive': 0, 'negative': 0},
+      'month': {'positive': 0, 'negative': 0},
+      'year': {'positive': 0, 'negative': 0},
     };
 
     final startOfYear = DateTime(_now.year, 1, 1);
@@ -50,35 +55,36 @@ class _CashOnHandPageState extends State<CashOnHandPage> {
       if (date.isAfter(startOfYear.subtract(const Duration(days: 1))) &&
           date.isBefore(endOfYear.add(const Duration(days: 1)))) {
         for (var event in events) {
-          final amount = (event.amount ?? 0) * (event.isPositiveCashflow ? 1 : -1);
+          final amount = event.amount ?? 0;
 
-          // Day total (cumulative from start of year to current day)
+          // Day totals
           if (!date.isAfter(_now)) {
-            _totals['day'] = (_totals['day'] ?? 0) + amount;
+            _updateTotals('day', amount, event.isPositiveCashflow);
           }
 
-          // Week total (cumulative from start of year to end of current week)
+          // Week totals
           if (!date.isAfter(_endOfWeek)) {
-            _totals['week'] = (_totals['week'] ?? 0) + amount;
+            _updateTotals('week', amount, event.isPositiveCashflow);
           }
 
-          // Month total (cumulative from start of year to end of current month)
+          // Month totals
           if (!date.isAfter(_endOfMonth)) {
-            _totals['month'] = (_totals['month'] ?? 0) + amount;
+            _updateTotals('month', amount, event.isPositiveCashflow);
           }
 
-          // // Half-year total
-          // if (date.year == _now.year &&
-          //     ((date.month <= 6 && _now.month <= 6) ||
-          //         (date.month > 6 && _now.month > 6))) {
-          //   _totals['halfYear'] = (_totals['halfYear'] ?? 0) + amount;
-          // }
-
-          // Year total
-          _totals['year'] = (_totals['year'] ?? 0) + amount;
+          // Year totals
+          _updateTotals('year', amount, event.isPositiveCashflow);
         }
       }
     });
+  }
+  
+  void _updateTotals(String period, double amount, bool isPositive) {
+    if (isPositive) {
+      _totals[period]!['positive'] = (_totals[period]!['positive'] ?? 0) + amount;
+    } else {
+      _totals[period]!['negative'] = (_totals[period]!['negative'] ?? 0) + amount;
+    }
   }
 
   @override
@@ -92,11 +98,10 @@ class _CashOnHandPageState extends State<CashOnHandPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            _buildTile('End of Day', _totals['day'] ?? 0),
-            _buildTile('End of Week', _totals['week'] ?? 0),
-            _buildTile('End of Month', _totals['month'] ?? 0),
-            // _buildTile('End of Half Year', _totals['halfYear'] ?? 0),
-            _buildTile('End of Year', _totals['year'] ?? 0),
+            _buildTile('End of Day', _totals['day']!, _now),
+            _buildTile('End of Week', _totals['week']!, _endOfWeek),
+            _buildTile('End of Month', _totals['month']!, _endOfMonth),
+            _buildTile('End of Year', _totals['year']!, _endOfYear),
           ],
         ),
       ),
@@ -113,30 +118,52 @@ class _CashOnHandPageState extends State<CashOnHandPage> {
     );
   }
 
-  Widget _buildTile(String title, double amount) {
-       final formattedAmount = amount < 0
-        ? '-\$${currencyFormatter.format(amount.abs())}'
-        : '\$${currencyFormatter.format(amount)}';    
-        final color = amount >= 0 ? Colors.green : Colors.red;
+
+
+  Widget _buildTile(String title, Map<String, double> amounts, DateTime date) {
+    final positiveAmount = amounts['positive'] ?? 0;
+    final negativeAmount = amounts['negative'] ?? 0;
+    final totalAmount = positiveAmount - negativeAmount;
 
     return Card(
       elevation: 4,
       margin: const EdgeInsets.symmetric(vertical: 8.0),
-      color: color.withOpacity(0.2),
-      child: ListTile(
-        title: Text(title),
-        trailing: Text(
-          formattedAmount,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: color,
-          ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+                        Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(title, style: Theme.of(context).textTheme.titleLarge),
+                Text(dateFormatter.format(date), style: Theme.of(context).textTheme.titleSmall),
+              ],
+            ),
+            const SizedBox(height: 8),
+            _buildAmountRow('Total Positive Cashflow', positiveAmount, Colors.green),
+            _buildAmountRow('Total Negative Cashflow', negativeAmount, Colors.red),
+            const Divider(),
+            _buildAmountRow('Total Cashflow', totalAmount, totalAmount >= 0 ? Colors.green : Colors.red),
+          ],
         ),
       ),
     );
   }
-}
 
+  Widget _buildAmountRow(String label, double amount, Color color) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(label),
+        Text(
+          amount < 0 ? '-\$${currencyFormatter.format(amount.abs())}' : '\$${currencyFormatter.format(amount)}',
+          style: TextStyle(fontWeight: FontWeight.bold, color: color),
+        ),
+      ],
+    );
+  }
+}
 
 
 // TODO: Implement filtering options for totals in the future
